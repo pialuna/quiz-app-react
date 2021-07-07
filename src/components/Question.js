33b0938369c260f987f92be96689fb754752ffc1
pialuna <pia.goldm@gmail.com>
2021-07-07 18:16:29 +0200

    ➕  Add headlessUI RadioGroup

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,33 @@
-import React from "react";
+import { React, useState } from "react";
 
-import Answer from "./Answer";
+import Option from "./Option";
 import Button from "./Button";
 
+import { RadioGroup } from "@headlessui/react";
+
 function Question() {
+  let [answer, setAnswer] = useState("Tokyo");
+
   return (
     <div>
       <h1 className={`text-center mb-4`}>Geography</h1>
       <h3 className={`text-center mb-4`}>
         What is the largest city in the world?
       </h3>
-      <Answer />
-      <Answer />
-      <Answer />
-      <Answer />
+
+      <RadioGroup value={answer} onChange={setAnswer}>
+        <RadioGroup.Label className="sr-only">Answer</RadioGroup.Label>
+        <RadioGroup.Option value="Tokyo">
+          {({ checked }) => <Option text="Tokyo" checked={checked} />}
+        </RadioGroup.Option>
+        <RadioGroup.Option value="Hong Kong">
+          {({ checked }) => <Option text="Hong Kong" checked={checked} />}
+        </RadioGroup.Option>
+        <RadioGroup.Option value="New York">
+          {({ checked }) => <Option text="New York" checked={checked} />}
+        </RadioGroup.Option>
+      </RadioGroup>
+
       <div className="w-full flex flex-row justify-center items-center">
         <Button text="back" />
         <Button text="next" />
