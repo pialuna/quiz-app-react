43c2a081e30fb83b0e7ec79264d980f346fb606b
pialuna <pia.goldm@gmail.com>
2021-07-07 21:38:28 +0200

    ➕  Add Apollo and GraphQL

--- src/index.js
+++ src/index.js
@@ -3,9 +3,42 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  //   useQuery,
+  //   gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://tilda-quiz.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetQuizzes {
+//         quizzes {
+//           id
+//           name
+//           questions {
+//             answer
+//             options
+//             text
+//           }
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
