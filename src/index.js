6f272d02580b8f5a49fa788e6cb8d307dc371104
pialuna <pia.goldm@gmail.com>
2021-07-08 20:47:33 +0200

    🚧  Reactive variable givenAnswersVar, trying out local-only fields

--- src/index.js
+++ src/index.js
@@ -7,13 +7,49 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  //   useQuery,
-  //   gql,
+  makeVar,
 } from "@apollo/client";
+
+// create reactive variable
+export const givenAnswersVar = makeVar([]);
 
 const client = new ApolloClient({
   uri: "https://tilda-quiz.hasura.app/v1/graphql",
   cache: new InMemoryCache(),
+  //   cache: new InMemoryCache({
+  //     // add score as local-only field
+  //     typePolicies: {
+  //       quizzes: {
+  //         fields: {
+  //           score: {
+  //             read(value, { readField }) {
+  //               //   const givenAnswers = givenAnswersVar().map(
+  //               //     (question) => {}
+  //               //   );
+
+  //               const correctlyAnsweredQuestions = givenAnswersVar().filter(
+  //                 (question) => question.correct === true
+  //               );
+  //               console.log(correctlyAnsweredQuestions);
+
+  //               const numberOfCorrectlyAnsweredQuestions =
+  //                 correctlyAnsweredQuestions.length;
+
+  //               console.log(numberOfCorrectlyAnsweredQuestions);
+
+  //               if (numberOfCorrectlyAnsweredQuestions > 0) {
+  //                 return `${correctlyAnsweredQuestions.length}/${
+  //                   givenAnswersVar().length
+  //                 }`;
+  //               } else {
+  //                 return "not started";
+  //               }
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   }),
 });
 
 // client
