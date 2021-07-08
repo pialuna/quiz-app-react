e046788a1d2df5d819b30f916892229cba918414
pialuna <pia.goldm@gmail.com>
2021-07-08 20:55:19 +0200

    🚧  Save given answers in reactive variable, check if correct

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,48 @@
 import { React, useState } from "react";
-
-import AnswerOption from "./AnswerOption";
+import { useReactiveVar } from "@apollo/client";
 
 import { RadioGroup } from "@headlessui/react";
+import AnswerOption from "./AnswerOption";
 
-function Question({ text, answer, options }) {
+import { givenAnswersVar } from "../index";
+
+function Question({ quizId, id, text, correctAnswer, options }) {
   // options to array
   const optionsArray = options.split(",");
-  console.log(optionsArray);
 
-  const [selectedOption, setSelectedOption] = useState(optionsArray[0]);
+  const [selectedOption, setSelectedOption] = useState("");
+  let givenAnswers = useReactiveVar(givenAnswersVar);
 
-  // TO DO: check answer
+  const saveAnswer = (answer) => {
+    setSelectedOption(answer);
+    // find the answer of the current question in the givenAnswers array
+    let currentQuestionAnswer = givenAnswers.find(
+      (question) => question.questionId === id
+    );
+    // if it was not answered yet, add to givenAnswers array
+    if (!currentQuestionAnswer) {
+      givenAnswers.push({
+        quizId: quizId,
+        questionId: id,
+        answer: answer,
+        isCorrect: answer === correctAnswer,
+      });
+    } else {
+      // update the answer
+      currentQuestionAnswer.answer = answer;
+      currentQuestionAnswer.isCorrect = answer === correctAnswer;
+    }
+
+    // update the reactive variable givenAnswersVar
+    givenAnswersVar(givenAnswers);
+    console.log(givenAnswersVar());
+  };
 
   return (
     <div>
       <h3 className={`text-center mb-4`}>{text}</h3>
 
-      <RadioGroup value={selectedOption} onChange={setSelectedOption}>
+      <RadioGroup value={selectedOption} onChange={saveAnswer}>
         <RadioGroup.Label className="sr-only">Answer</RadioGroup.Label>
         {optionsArray.map((option, index) => {
           return (
