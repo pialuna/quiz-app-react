e6b537684ba877ef60feb0c25fc8a6433e3411d7
pialuna <pia.goldm@gmail.com>
2021-07-09 13:50:59 +0200

    ♻️  Clean up index.js, remove local-only field experiements

--- src/index.js
+++ src/index.js
@@ -16,59 +16,7 @@ export const givenAnswersVar = makeVar([]);
 const client = new ApolloClient({
   uri: "https://tilda-quiz.hasura.app/v1/graphql",
   cache: new InMemoryCache(),
-  //   cache: new InMemoryCache({
-  //     // add score as local-only field
-  //     typePolicies: {
-  //       quizzes: {
-  //         fields: {
-  //           score: {
-  //             read(value, { readField }) {
-  //               //   const givenAnswers = givenAnswersVar().map(
-  //               //     (question) => {}
-  //               //   );
-
-  //               const correctlyAnsweredQuestions = givenAnswersVar().filter(
-  //                 (question) => question.correct === true
-  //               );
-  //               console.log(correctlyAnsweredQuestions);
-
-  //               const numberOfCorrectlyAnsweredQuestions =
-  //                 correctlyAnsweredQuestions.length;
-
-  //               console.log(numberOfCorrectlyAnsweredQuestions);
-
-  //               if (numberOfCorrectlyAnsweredQuestions > 0) {
-  //                 return `${correctlyAnsweredQuestions.length}/${
-  //                   givenAnswersVar().length
-  //                 }`;
-  //               } else {
-  //                 return "not started";
-  //               }
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   }),
 });
-
-// client
-//   .query({
-//     query: gql`
-//       query GetQuizzes {
-//         quizzes {
-//           id
-//           name
-//           questions {
-//             answer
-//             options
-//             text
-//           }
-//         }
-//       }
-//     `,
-//   })
-//   .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
